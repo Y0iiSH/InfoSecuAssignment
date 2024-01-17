254947fd9ab173e58239435377a3e6a325b8a5d7
Amirul Shafiq <mirullshaffiq01@gmail.com>
2024-01-17 17:22:14 +0800

    improve password

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'VMS API',
+            title: 'Vistor Management System',
             version: '1.0.0'
         },
         components: {  // Add 'components' section
